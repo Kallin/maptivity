b8077eed528c03e843eef5c1d386acf254045010
knagelberg <knagelberg@salesforce.com>
2013-09-20 14:51:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maptivity::Application.config.secret_key_base = '0c9441adbfd77e7eec5c9b3b7af8a66980960af32545e58ed85b0f22192169e60b703e8613624cafda9669d92b799613907ac696d3fef8fdd413878c7df04d10'
